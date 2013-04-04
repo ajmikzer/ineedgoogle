eb6d70a0b263301090e2908831be6426c101c963
ajmikzer <hayastan2007>
2013-04-04 21:32:09 +0400

    Adding files
    Adding source files

---
+++ src/itu-map-base.js
@@ -0,0 +1,63 @@
+var itu_base_style_light = [
+ {
+   "stylers": [
+     { "saturation": -100 },
+     { "lightness": -11 }
+   ]
+ },{
+   "featureType": "water",
+   "stylers": [
+     { "color": "#fbfbfb" }
+   ]
+ },{
+   "featureType": "road.local",
+   "stylers": [
+     { "visibility": "off" }
+   ]
+ },{
+   "featureType": "road.arterial",
+   "stylers": [
+     { "visibility": "off" }
+   ]
+ },{
+   "featureType": "road.highway",
+   "stylers": [
+     { "visibility": "off" }
+   ]
+ },{
+   "featureType": "transit.station",
+   "stylers": [
+     { "visibility": "off" }
+   ]
+ },{
+   "featureType": "transit.line",
+   "stylers": [
+     { "visibility": "off" }
+   ]
+ },{
+   "featureType": "poi.park",
+   "stylers": [
+     { "visibility": "off" }
+   ]
+ },{
+   "featureType": "administrative.land_parcel",
+   "stylers": [
+     { "visibility": "off" }
+   ]
+ },{
+   "featureType": "administrative.neighborhood",
+   "stylers": [
+     { "visibility": "off" }
+   ]
+ },{
+   "featureType": "administrative.province",
+   "stylers": [
+     { "visibility": "off" }
+   ]
+ },{
+   "featureType": "administrative.locality",
+   "stylers": [
+     { "visibility": "off" }
+   ]
+ }
+];
